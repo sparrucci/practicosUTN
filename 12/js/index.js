2e472e9e04e2adf86112d950f92fcfeb5be35b5e
Silvana <sparrucci@gmail.com>
2026-09-12 22:22:50 -0300

    Agrego mas esjecicios

---
+++ 12/js/index.js
@@ -0,0 +1,14 @@
+console.log("Hola mundo")
+console.log(2+2)
+
+let nombreAlumno
+let nombre = "Silvana"
+
+nombreAlumno = "Pedro"
+console.log(nombreAlumno, nombre)
+
+nombreAlumno=20
+console.log(nombreAlumno)
+
+nombreAlumno=35
+console.log(nombreAlumno*20)
---
+++ 12/js/variables.js
@@ -0,0 +1,28 @@
+var colorTaza ="gris"
+
+colorTaza = "rojo"
+
+var colorTaza = "azul"
+
+console.log(colorTaza)
+
+let puntaje = 0
+
+puntaje = 10
+puntaje = 25
+console.log(puntaje)
+
+// let puntaje = 10
+// console.log(puntaje)
+
+const fechaNacimiento = "1990"
+
+//fechaNacimiento = "1995"
+
+console.log(fechaNacimiento)
+
+const nombres = ["Silvana", "Pedro", "Juan"]
+
+nombres.push("Maria")
+console.log(nombres)
+
